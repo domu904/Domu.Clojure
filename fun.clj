955007546fdfed671483a913755cf71e37fd0a34
domu904 <domu904@gmail.com>
2014-04-03 15:10:09 -0500

    adding clojure file

---
+++ fun.clj
@@ -0,0 +1,43 @@
+;; Anything you type in here will be executed
+;; immediately with the results shown on the
+;; right.
+;;(if true "yes" "no")
+
+;;(def the-answer 42)
+
+;;(def my-stuff [1 2 3])
+
+;;(type my-stuff)
+
+
+;;(println (list 1 2 3))
+
+(ns domu)
+
+(str "Hello world")
+
+
+(if true "yes" "no")
+
+
+(eval '(+ 1 2))
+
+(fn [] "Hello World")
+
+((fn [] "Hello World"))
+
+
+(def x 1)
+
+x;
+
+
+
+
+(defn hello-world [] "Hello World")
+
+(hello-world)
+
+
+
+
